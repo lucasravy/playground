30a1522230032a237791e35eb04df10fcc33f443
Lucas Ravy Londe <lucaslonde@gmail.com>
2022-03-30 20:05:27 -0300

    Add Time Complexity lesson missing element

---
+++ codility/src/03-Time-Complexity/perm-missing-elem/perm-missing-elem.js
@@ -0,0 +1,19 @@
+function solution (A) {
+    // empty-array
+    if (A.length === 0) return 1
+    arr = A.sort((a, b) => a - b)
+    // valued-array
+    for (let i = 0; i < arr.length -1 ; i++) {
+        const a = arr[i]
+        const b = arr[i + 1]
+        if (b - a != 1 ) {
+            return a + 1
+        }
+    }
+    // missing-first
+    if (A[0] === 2) return 1
+    // missing-last
+    return A[A.length - 1] + 1
+}
+
+module.exports = solution
---
+++ codility/src/03-Time-Complexity/perm-missing-elem/perm-missing-elem.test.js
@@ -0,0 +1,33 @@
+const solution = require('./perm-missing-elem')
+
+describe('Permanent Missing Element Tests', () => {
+    describe('Correction Tests', () => {
+        test('should return 4 for missing element in [2, 3, 1, 5]', () => {
+            expect(solution([2,3,1,5])).toBe(4)
+        })
+
+        test('should return 1 for an empty request', () => {
+            expect(solution([])).toBe(1)
+        })
+
+        test('should return 1 for de missing element in [1]', () => {
+            expect(solution([1])).toBe(2)
+        })
+
+        test('should return 6 for de missing element in [1, 2, 3, 4, 5]', () => {
+            expect(solution([1, 2, 3, 4, 5])).toBe(6)
+        })
+
+        test('should return 1 for de missing element in [2, 3, 4, 5, 6]', () => {
+            expect(solution([2, 3, 4, 5, 6])).toBe(1)
+        })
+
+        test('should return 1 for de missing element in [2]', () => {
+            expect(solution([2])).toBe(1)
+        })
+
+        test('should return 1 for de missing element in [3, 2]', () => {
+            expect(solution([3, 2])).toBe(1)
+        })
+    })
+})
